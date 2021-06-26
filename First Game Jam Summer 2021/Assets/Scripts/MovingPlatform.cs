5ffd7f849eb46c9a4dcc50cf78d03ffdc44a6137
Diamondsword0 <diamondsword19@gmx.com>
2021-06-26 14:31:38 -0500

    Platform script
    v3

--- First Game Jam Summer 2021/Assets/Scripts/MovingPlatform.cs
+++ First Game Jam Summer 2021/Assets/Scripts/MovingPlatform.cs
@@ -19,7 +19,20 @@ public class MovingPlatform : MonoBehaviour
     {
         if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
         {
-
+            i++;
+            if (i == points.Length)
+            {
+                i = 0;
+            }
         }
+
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
     }
+
+
+
+
+
+
+
 }
