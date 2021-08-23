0fb917c1e88100e126e81559db43ea1e2953ede8
ObsoleteBear <pauldavidreaves@gmail.com>
2021-08-23 16:53:39 -0500

    Movement

--- Brackeyes game jam/Assets/Scripts/Controller.cs
+++ Brackeyes game jam/Assets/Scripts/Controller.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class Controller : MonoBehaviour
 {
     public float Speed = 5f;
+    public Rigidbody2D rb;
 
     void Update()
     {
         Vector2 Userinput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        transform.Translate(Userinput * Speed * Time.deltaTime);
+        //transform.Translate(Userinput * Speed * Time.deltaTime);
+
+        rb.velocity = Userinput * Speed;
     }
 }
 
