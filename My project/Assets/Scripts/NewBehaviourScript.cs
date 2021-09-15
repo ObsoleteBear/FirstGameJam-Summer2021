ef9b8d4ae48d9ec7e06d603a2762efdbf73017b8
Diamondsword0 <diamondsword19@gmx.com>
2021-09-15 17:33:38 -0500

    asd

---
+++ My project/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float speed = 1.0f;
+    double health = 100.0;
+}
